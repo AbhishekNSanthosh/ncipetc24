edc5563455f6a295cfe17730821fe379b2254f05
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-02-21 12:09:13 +0530

    feat:responsive

--- src/widgets/ImportantDates/view.jsx
+++ src/widgets/ImportantDates/view.jsx
@@ -11,6 +11,9 @@ export default function ImportantDates() {
       <div className={styles.wrapper}>
         <Image src="/images/impDates.svg" width={1000} height={700}/>
       </div>
+      <div className={styles.wrapperMob}>
+        <Image src="/images/impDatesMob.svg" width={800} height={700} className={styles.date}/>
+      </div>
     </div>
   )
 }
