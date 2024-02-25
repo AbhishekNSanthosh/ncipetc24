8166a1789ad2de2f128da651ab765fa535b2c76a
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-02-25 07:28:52 +0530

    refactor:important dates

--- src/widgets/ImportantDates/view.jsx
+++ src/widgets/ImportantDates/view.jsx
@@ -8,10 +8,10 @@ export default function ImportantDates() {
     <div className={styles.container}>
       <Banner title={"IMPORTANT DATES"} />
       <div className={styles.wrapper}>
-        <Image src="/images/impDates.svg" width={1000} height={700} />
+        <Image src="/images/impDates.png" width={1000} height={700} />
       </div>
       <div className={styles.wrapperMob}>
-        <Image src="/images/impDatesMob.svg" width={800} height={700} className={styles.date} />
+        <Image src="/images/impDatesMob.png" width={800} height={700} className={styles.date} />
       </div>
     </div>
   )
