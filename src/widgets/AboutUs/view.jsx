45bef9b732c764930a148b38783f1dcfe1a153e4
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-02-28 15:38:05 +0530

    feat:not-found

--- src/widgets/AboutUs/view.jsx
+++ src/widgets/AboutUs/view.jsx
@@ -3,6 +3,7 @@ import styles from '@styles/scss/about.module.scss'
 import { aboutUsData } from '../../common/constants/constants'
 import Image from 'next/image'
 import Banner from '@components/Banner'
+import RockerIcon from '@icons/RockerIcon'
 
 export default function AboutUs() {
   return (
@@ -24,29 +25,80 @@ export default function AboutUs() {
               </div>
             </div>
           ))}
-          <div className={styles.missionRow}>
-            <div className={styles.missionTitle}>
-              <span className={styles.title}>Department Mission</span>
-            </div>
-            <div className={styles.missionRow}>
-              <div className={styles.missionTitle}>
-                <p className={styles.desc}>To nurture professionally competent and socially committed IT professionals</p>
+          <div className={styles.detailsRow}>
+            <div className={styles.col}>
+              <div className={styles.missionRow}>
+                <div className={styles.missionTitle}>
+                  <span className={styles.title}>Insitute Mission</span>
+                </div>
+                <div className={styles.missionRow}>
+                  <div className={styles.missionTitle}>
+                    <p className={styles.desc}>To mould distinctive engineers with integrity and social commitment.</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.missionRow}>
+                <div className={styles.missionTitle}>
+                  <span className={styles.title}>Institute Vision</span>
+                </div>
+                <div className={styles.visionRow}>
+                  <div className={styles.missionTitle}>
+                    <div className={styles.iconbox}>
+                      <RockerIcon />
+                    </div>
+                    <p className={styles.desc}>Extend harmonious curricular and co-curricular exposure to the students.</p>
+                  </div>
+                  <div className={styles.missionTitle}>
+                    <div className={styles.iconbox}>
+                      <RockerIcon />
+                    </div>
+                    <p className={styles.desc}>Equip the students to accomplish career goals upholding moral values.</p>
+                  </div>
+                  <div className={styles.missionTitle}>
+                    <div className={styles.iconbox}>
+                      <RockerIcon />
+                    </div>
+                    <p className={styles.desc}>Encourage the students to contribute for the sustainable development of the society.</p>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={styles.missionRow}>
-            <div className={styles.missionTitle}>
-              <span className={styles.title}>Department Vision</span>
-            </div>
-            <div className={styles.visionRow}>
-              <div className={styles.missionTitle}>
-                <p className={styles.desc}>Provide quality education in both theoritical and applied fields of computer science.</p>
+
+            <div className={styles.col}>
+              <div className={styles.missionRow}>
+                <div className={styles.missionTitle}>
+                  <span className={styles.title}>Department Mission</span>
+                </div>
+                <div className={styles.missionRow}>
+                  <div className={styles.missionTitle}>
+                    <p className={styles.desc}>To nurture professionally competent and socially committed IT professionals</p>
+                  </div>
+                </div>
               </div>
-              <div className={styles.missionTitle}>
-                <p className={styles.desc}>Equip the students with industry-oriented skills in  computer science to take up real-world challenges .</p>
-              </div>
-              <div className={styles.missionTitle}>
-                <p className={styles.desc}>Inculcate ethical values and leadership qualities.</p>
+              <div className={styles.missionRow}>
+                <div className={styles.missionTitle}>
+                  <span className={styles.title}>Department Vision</span>
+                </div>
+                <div className={styles.visionRow}>
+                  <div className={styles.missionTitle}>
+                    <div className={styles.iconbox}>
+                      <RockerIcon />
+                    </div>
+                    <p className={styles.desc}>Provide quality education in both theoritical and applied fields of computer science.</p>
+                  </div>
+                  <div className={styles.missionTitle}>
+                    <div className={styles.iconbox}>
+                      <RockerIcon />
+                    </div>
+                    <p className={styles.desc}>Equip the students with industry-oriented skills in  computer science to take up real-world challenges .</p>
+                  </div>
+                  <div className={styles.missionTitle}>
+                    <div className={styles.iconbox}>
+                      <RockerIcon />
+                    </div>
+                    <p className={styles.desc}>Inculcate ethical values and leadership qualities.</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- src/widgets/Contact/view.jsx
+++ src/widgets/Contact/view.jsx
@@ -5,7 +5,20 @@ import Banner from '@components/Banner'
 export default function Contact() {
   return (
     <div className={styles.container}>
-      <Banner title={"Contact us"}/>
+      <Banner title={"Contact us"} />
+      <div className={styles.wrapper}>
+        <div className={styles.row}>
+          <div className={styles.box}>
+a
+          </div>
+          <div className={styles.box}>
+a
+          </div>
+          <div className={styles.box}>
+a
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
