1dc125d50d3b107877903945ceff1f9344e688d7
Abhishek N Santhosh <97347843+AbhishekNSanthosh@users.noreply.github.com>
2024-03-02 10:04:45 +0530

    Refactor: about us

--- src/common/constants/constants.js
+++ src/common/constants/constants.js
@@ -76,7 +76,7 @@ export const aboutUsData = [
     },
     {
         title: "About department",
-        desc: "The Department of CSE in Carmel College of Engineering and Technology was established in 2017 with a maximum intake of 60 students per year. Department plays a key role to enhance the learning and technical competence of students in the field of Computer Science & Engineering. Workshops and hands-on sessions are conducted on a regular basis. The Department frequently conducts seminars and invited talks with the support of experienced resource persons from the industry and well known academic institutions. Department has signed MoU with various IT legends and International Universities.",
+        desc: "The Department of CSE in Carmel College of Engineering and Technology was established in 2017 with a maximum intake of 60 students per year. Department plays a key role to enhance the learning and technical competence of students in the field of Computer Science & Engineering. Workshops and hands-on sessions are conducted on a regular basis. The Department frequently conducts seminars and invited talks with the support of experienced resource persons from the industry and well known academic institutions. Department has signed MoU with various IT legends and International Universities. The faculty members pursue research in areas like Computer Networking,Cloud computing, Image processing,Computer Vision,. Our students are also encouraged to publish their research work in public domain and guides them to present their papers in National and International conferences.",
         img: "/images/dep.png",
         reverse: false
     }
